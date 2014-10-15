dc4013c99b97156ebe0e2f4595e81308c7c19cc7
Mike Vanston <mvanston@enmasse.com>
2014-10-15 12:35:09 -0700

    Updated

--- Code/map_script.vb
+++ Code/map_script.vb
@@ -6,27 +6,27 @@ Dim WS As Worksheet
 Dim MatchCase As Boolean
 With ActiveSheet.UsedRange
 
-.Replace "MarsGameHorde", "Assault", xlPart
-.Replace "{MarsGameHorde8P}", "Massive Assault", xlPart
-.Replace "MarsGameSurvival", "Kill Every Thing", xlPart
-.Replace "MarsGameSurvival2", "Kill Every Thing 2", xlPart
-.Replace "MarsGameAST", "Assault Operations", xlPart
-.Replace "MarsGameZAST", "Paranormal Operations", xlPart
-.Replace "MarsGameDefence", "Threshold Defense", xlPart
-.Replace "MarsGameMonster", "Extinction Ops", xlPart
-.Replace "MarsGameCM", "Training", xlPart
-.Replace "MarsGameQRM", "Team Deathmatch", xlPart
-.Replace "MarsGameTDM", "Elimination", xlPart
-.Replace "MarsGameDM", "Free-for-All", xlPart
-.Replace "MarsGameAnnex", "King of the Hill", xlPart
-.Replace "MarsGameDefuse", "Demolition", xlPart
-.Replace "MarsGameBighead", "Head Hunter", xlPart
-.Replace "MarsGameMVZ", "Mercs vs. Monsters", xlPart
-.Replace "MarsGameInfection", "Patient Zero", xlPart
+.Replace "MarsGameHorde", "Assault"
+.Replace "{MarsGameHorde8P}", "Massive Assault"
+.Replace "MarsGameSurvival", "Kill Every Thing"
+.Replace "MarsGameSurvival2", "Kill Every Thing 2"
+.Replace "MarsGameAST", "Assault Operations"
+.Replace "MarsGameZAST", "Paranormal Operations"
+.Replace "MarsGameDefence", "Threshold Defense"
+.Replace "MarsGameMonster", "Extinction Ops"
+.Replace "MarsGameCM", "Training"
+.Replace "MarsGameQRM", "Team Deathmatch"
+.Replace "MarsGameTDM", "Elimination"
+.Replace "MarsGameDM", "Free-for-All"
+.Replace "MarsGameAnnex", "King of the Hill"
+.Replace "MarsGameDefuse", "Demolition"
+.Replace "MarsGameBighead", "Head Hunter"
+.Replace "MarsGameMVZ", "Mercs vs. Monsters"
+.Replace "MarsGameInfection", "Patient Zero"
 
 For Each WS In Worksheets
         WS.Cells.Replace What:=Search, Replacement:=Replacement, _
-        LookAt:=xlPart, MatchCase:=False
+        LookAt:=xlPart, MatchCase:=True
 Next
 End With
 End Sub
