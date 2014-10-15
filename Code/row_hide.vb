e1ba59004983f30954e0384bb77616d600b92806
Mike Vanston <mvanston@enmasse.com>
2014-10-15 15:37:03 -0700

    Updated some stuff

--- Code/row_hide.vb
+++ Code/row_hide.vb
@@ -2,16 +2,23 @@ Sub RangeHide()
     Set rColumnD = Range("D2", "D75")
     Set rColumnE = Range("E2", "E75")
     
-    For Each cell In rColumnD
-        If cell.Value = "{}" Then
-            Rows(cell.Row).EntireRow.Hidden = True
+    For Each Row In Worksheet
+        If ((rColumnD.cell.Value = "{}") And (rColumnE.cell.Value = "{}")) Then
+            Row.EntireRow.Hidden = True
         End If
+    Next
+    
+    'For Each cell In rColumnD
+        'If (cell.Value = "{}") Then
+            'Rows(cell.Row).EntireRow.Hidden = True
+        
+        'Else
         
     'For Each cell In rColumnE
         'If cell.Value = "{}" Then
             'Rows(cell.Row).EntireRow.Hidden = True
         'End If
         
-    Next
+    'Next
  
 End Sub
