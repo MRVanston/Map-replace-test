6bee608ffa642efa7c1da7a45e6e09c263cb1e45
Mike Vanston <mvanston@enmasse.com>
2014-10-15 14:58:27 -0700

    Adding

---
+++ Code/row_hide.vb
@@ -0,0 +1,17 @@
+Sub RangeHide()
+    Set rColumnD = Range("D2", "D75")
+    Set rColumnE = Range("E2", "E75")
+    
+    For Each cell In rColumnD
+        If cell.Value = "{}" Then
+            Rows(cell.Row).EntireRow.Hidden = True
+        End If
+        
+    'For Each cell In rColumnE
+        'If cell.Value = "{}" Then
+            'Rows(cell.Row).EntireRow.Hidden = True
+        'End If
+        
+    Next
+ 
+End Sub
